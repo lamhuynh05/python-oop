d21d394cb394c555c3f7bb94017a41e9f2ac5122
lamhuynh05 <huynhngoclam2005@gmail.com>
2023-11-18 15:08:42 +0700

    .

---
+++ car.py
@@ -0,0 +1,20 @@
+# when class is large -> place class into separate module
+class Car:
+# special method __init__(): construct objects/constructor
+# init = initialize 
+# set up parameters inside the ()
+# assign received arguments to attributes in __init__ method -> 
+    def __init__(self, make, model, year, color):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.color = color
+        # these are attributes
+        # attributes can be anything since self.make = make
+
+# define methods for object car:
+    def drive(self): # self is argument, refers to object using this method
+        print("This "+self.model+ " is driving")
+
+    def stop(self):
+        print("This "+self.model+ " is stopped!")
