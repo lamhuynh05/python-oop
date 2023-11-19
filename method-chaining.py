601b241d183ef78f4fcfd64643a83e3687f41342
lamhuynh05 <huynhngoclam2005@gmail.com>
2023-11-20 01:50:27 +0700

    Create method-chaining.py

---
+++ method-chaining.py
@@ -0,0 +1,15 @@
+class Car:
+    def turn_on(self):
+        print("You start the engine")
+
+    def drive(self):
+        print("You drive the car")
+
+    def brake(self):
+        print("You step on the brakes")
+
+    def turn_off(self):
+        print("You turn off the engines")
+
+car = Car()
+
