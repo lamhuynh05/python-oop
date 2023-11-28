f60a599c4216fb8852b6767d6c73c18647436198
lamhuynh05 <huynhngoclam2005@gmail.com>
2023-11-28 10:43:44 +0700

    Update method-chaining.py

--- method-chaining.py
+++ method-chaining.py
@@ -1,15 +1,30 @@
 class Car:
     def turn_on(self):
         print("You start the engine")
+        return self
 
     def drive(self):
         print("You drive the car")
+        return self
 
     def brake(self):
         print("You step on the brakes")
+        return self
 
     def turn_off(self):
         print("You turn off the engines")
+        return self
 
 car = Car()
 
+# call 2 methods at once
+car.turn_on().drive()
+
+# # call all 4 methods 
+car.turn_on().drive().brake().turn_off()
+
+# if we call many methods:
+car.turn_on()\
+    .drive()\
+    .brake()\
+    .turn_off()
